bd9bc0d55ec6d1622d856e60eb0190454255b272
Phani Vankadari <phani.kumar.vankadari@gmail.com>
2019-05-14 13:24:45 +0000

    init commit

---
+++ controller/App.controller.js
@@ -0,0 +1,1 @@
+sap.ui.define([   "sap/ui/core/mvc/Controller",   "sap/m/MessageToast"], function (Controller, MessageToast) {   "use strict";   return Controller.extend("sap.ui5.exer2.controller.App", {      onShowHello : function () {         MessageToast.show("Hello World");      }   });});
